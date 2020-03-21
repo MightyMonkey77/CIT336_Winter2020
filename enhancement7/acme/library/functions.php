1111f7a43cbae212ca10d66c1c70f59dbf19b898
Payne, Daryl <pay15007@byui.edu>
2020-03-20 19:43:50 -0700

    Enhancement 7

---
+++ enhancement7/acme/library/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Functions 
+*/
+
+
+//Validation for Email
+function checkEmail($clientEmail){
+    $valEmail = filter_var($clientEmail, FILTER_VALIDATE_EMAIL);
+    return $valEmail;
+}
+
+// Check the password for a minimum of 8 characters,
+// at least one 1 capital letter, at least 1 number and
+// at least 1 special character
+function checkPassword($clientPassword){
+    $pattern = '/^(?=.*[[:digit:]])(?=.*[[:punct:]])(?=.*[A-Z])(?=.*[a-z])([^\s]){8,}$/';
+    return preg_match($pattern, $clientPassword);
+}
+
+// Build a navigation bar using the $categories array dynamically
+function navList() {
+    $categories = getCategories();
+    $navList = '<ul>';
+    $navList .= "<li><a href='../index.php' title='View the Acme home page'>Home</a></li>";
+    foreach ($categories as $category) {
+     $navList .= "<li><a href='../index.php?action=".urlencode($category['categoryName'])."' title='View our $category[categoryName] product line'>$category[categoryName]</a></li>";
+    }
+    $navList .= '</ul>';
+    return $navList;
+}
+
+// Build the categories select list 
+function buildCategoryList($categoriesAndIds){ 
+    //$categories = getCategories();
+    $catList = '<select name="categoryId" id="categoryList" class="list2">'; 
+    $catList .= "<option>Choose a Category</option>"; 
+    foreach ($categoriesAndIds as $category) { 
+     $catList .= "<option value='$category[categoryId]'>$category[categoryName]</option>"; 
+    } 
+    $catList .= '</select>'; 
+    return $catList; 
+}
